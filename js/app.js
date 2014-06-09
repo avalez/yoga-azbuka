85e0793d4bb73dcfeb3fa96801933225175af7cc
Andriy Zhdanov <azhdanov@gmail.com>
2014-06-09 16:32:07 +0200

    create

---
+++ js/app.js
@@ -0,0 +1,49 @@
+$(function() {
+  $('#ri-grid').gridrotator({
+    w1024: {rows: 2, columns: 3},
+    w768 : {rows: 2, columns: 3},
+    w480 : {rows: 2, columns: 3},
+    w320 : {rows: 3, columns: 4},
+    w240 : {rows: 3, columns: 3}
+  });
+
+  var loading = function(isLoading) {
+    var $e = $('#loading');
+    isLoading === true ? $e.show() : $e.hide();
+  }
+
+  var submitted = false;
+  var $form = $('#demoLicense form');
+  $form.validate({
+    submitHandler: function(form) {
+      if (submitted) {
+        return;
+      }
+      submitted = true;
+      loading(true);
+      $.ajax({
+        url: form.action,
+        cache: false,
+        type: 'POST',
+        data: $form.serialize(),
+        dataType: "jsonp",
+        //timeout: 1000,
+        crossDomain: true,
+        success: function (data, status) {
+            if (data == 'OK') {
+                $form.parent().html("You should recieve license to specified email shortly.")
+            } else {
+                $form.parent().html("Error: " + data);
+            }
+        },
+        error: function (xOptions, textStatus) {
+            submitted = false;
+            console.log(textStatus);
+        },
+        complete: function() {
+            loading(false);
+        }
+      });
+    }
+  });
+});
