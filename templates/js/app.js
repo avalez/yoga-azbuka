8b6d1c2da508385979408ba740975fc16686f3be
Andriy Zhdanov <azhdanov@gmail.com>
2015-12-06 21:36:35 +0100

    update

--- templates/js/app.js
+++ templates/js/app.js
@@ -296,8 +296,9 @@ $(function() {
           var weightFactor = Math.max(0, Math.floor((poster + cards - 1) / 2));
           if (delivery.text == 'Почта') {
               if (poster > 0) {
-                  cost = 100 + (this.isAddressRussia() ? 250 : 1000);
-              } else if (cards > 0) {
+                  cost = this.isAddressRussia() ? 150 : 350);
+              }
+              if (cards > 0) {
                   cost = cards > 4 ? 50 : 20;
                   cost += this.isAddressRussia() ? 100 : 300;
               }
